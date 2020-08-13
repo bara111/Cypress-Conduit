58b2e9831c09bdf3d38fcc6a08306d7d7d017b9a
BaraA <am.bara059@gmail.com>
2020-08-13 09:11:11 +0300

    testing adding success test to test new article

--- cypress/integration/conduit/user/NewArticle/new_article_spec.js
+++ cypress/integration/conduit/user/NewArticle/new_article_spec.js
@@ -1,4 +1,4 @@
-import * as Errors from "./constants";
+import * as Errors from './constants'
 describe(
 	'testing writing new Article',
 	{ baseUrl: 'https://demo.productionready.io/#/' },
@@ -6,14 +6,18 @@ describe(
 		beforeEach(() => {
 			cy.login('am.bara059@gmail.com', '123456789')
 			cy.visit('/editor/')
-			cy.get('input[placeholder=\"Article Title\"]').as('articleTitleField')
-			cy.get('input[placeholder=\"What\'s this article about?\"]').as('articleDescriptionField')
-			cy.get('input[placeholder=\"Write your article (in markdown)\"]').as('articlesBodyField')
-			cy.get('input[placeholder=\"Enter tags\"').as('tagsField')
+			cy.get('input[placeholder="Article Title"]').as('articleTitleField')
+			cy.get('input[placeholder="What\'s this article about?"]').as(
+				'articleDescriptionField'
+			)
+			cy.get("textarea[placeholder='Write your article (in markdown)']").as(
+				'articlesBodyField'
+			)
+			cy.get('input[placeholder="Enter tags"').as('tagsField')
 			cy.get('.btn').as('publishArticleButton')
+			cy.get("a[ui-sref*='username']").as('profileLink')
 		})
 		it('testing visiting new Article for logged users', () => {
-			cy.url().should('eq', 'https://demo.productionready.io/#/editor/')
 			cy.get('@articleTitleField')
 			cy.get('@articleDescriptionField')
 			cy.get('@articlesBodyField')
@@ -21,7 +25,6 @@ describe(
 			cy.get('@publishArticleButton')
 		})
 		it('testing article Title Field is empty', () => {
-			cy.url().should('eq', 'https://demo.productionready.io/#/editor/')
 			cy.get('@articleTitleField')
 			cy.get('@articleDescriptionField').type('anyThingisAbout')
 			cy.get('@articlesBodyField').type('anyThingisAbout')
@@ -30,41 +33,31 @@ describe(
 			cy.contains(Errors.TITLE_FILED_CAN_NOT_BE_BLANK)
 			cy.contains(Errors.TITLE_FILED_IS_TOO_SHORT)
 			cy.contains(Errors.DESCRIPTION_FILED_CAN_NOT_BE_BLANK).should('not.exist')
-			cy.contains(Errors.DESCRIPTION_IS_TOO_SHORT).should(
-				'not.exist'
-			)
+			cy.contains(Errors.DESCRIPTION_IS_TOO_SHORT).should('not.exist')
 			cy.contains(Errors.BODY_FILED_CAN_NOT_BE_BLANK).should('not.exist')
 		})
 		it('testing  article description Field is empty', () => {
-			cy.url().should('eq', 'https://demo.productionready.io/#/editor/')
 			cy.get('@articleTitleField').type('anyThingisAbout')
 			cy.get('@articleDescriptionField')
 			cy.get('@articlesBodyField').type('anyThingisAbout')
 			cy.get('@tagsField').type('anyThingisAbout')
 			cy.get('@publishArticleButton').click()
 			cy.contains(Errors.TITLE_FILED_CAN_NOT_BE_BLANK).should('not.exist')
-			cy.contains(Errors.TITLE_FILED_IS_TOO_SHORT).should(
-				'not.exist'
-			)
+			cy.contains(Errors.TITLE_FILED_IS_TOO_SHORT).should('not.exist')
 			cy.contains(Errors.DESCRIPTION_FILED_CAN_NOT_BE_BLANK)
 			cy.contains(Errors.DESCRIPTION_IS_TOO_SHORT)
 			cy.contains(Errors.BODY_FILED_CAN_NOT_BE_BLANK).should('not.exist')
 		})
 		it('testing article body Field is empty', () => {
-			cy.url().should('eq', 'https://demo.productionready.io/#/editor/')
 			cy.get('@articleTitleField').type('anyThingisAbout')
 			cy.get('@articleDescriptionField').type('anyThingisAbout')
 			cy.get('@articlesBodyField')
 			cy.get('@tagsField').type('anyThingisAbout')
 			cy.get('@publishArticleButton').click()
 			cy.contains(Errors.TITLE_FILED_CAN_NOT_BE_BLANK).should('not.exist')
-			cy.contains(Errors.TITLE_FILED_IS_TOO_SHORT).should(
-				'not.exist'
-			)
+			cy.contains(Errors.TITLE_FILED_IS_TOO_SHORT).should('not.exist')
 			cy.contains(Errors.DESCRIPTION_FILED_CAN_NOT_BE_BLANK).should('not.exist')
-			cy.contains(Errors.DESCRIPTION_IS_TOO_SHORT).should(
-				'not.exist'
-			)
+			cy.contains(Errors.DESCRIPTION_IS_TOO_SHORT).should('not.exist')
 			cy.contains("body can't be blank")
 		})
 		it('test for fields to be disabled after submit', () => {
@@ -79,6 +72,20 @@ describe(
 			cy.get('@articlesBodyField').should('be.disabled')
 			cy.get('@tagsField').should('be.disabled')
 			cy.get('@publishArticleButton').should('be.disabled')
+		})
+
+		it('testing success writing new article', () => {
+			cy.get('@articleTitleField').type('article title')
+			cy.get('@articleDescriptionField').type('anyThingisAbout')
+			cy.get('@articlesBodyField').type('articlebody')
+			cy.get('@tagsField').type('anyThingisAbout')
+			cy.get('@publishArticleButton').click()
+			cy.contains(Errors.TITLE_FILED_CAN_NOT_BE_BLANK).should('not.exist')
+			cy.contains(Errors.TITLE_FILED_IS_TOO_SHORT).should('not.exist')
+			cy.contains(Errors.DESCRIPTION_FILED_CAN_NOT_BE_BLANK).should('not.exist')
+			cy.contains(Errors.DESCRIPTION_IS_TOO_SHORT).should('not.exist')
+			cy.contains(Errors.BODY_FILED_CAN_NOT_BE_BLANK).should('not.exist')
+			cy.contains('article title')
 		})
 	}
 )
